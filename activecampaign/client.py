320ff00ad5e17e6ab7fdcd6bf603d46a70a811ff
sdamyanov <stefan@e-motive.online>
2023-07-07 19:45:27 +0300

    added new class Brandings

--- activecampaign/client.py
+++ activecampaign/client.py
@@ -15,6 +15,7 @@ from .emailActivities import EmailActivities
 from .dealActivities import DealActivities
 from .customobjects import CustomObjects
 from .campaigns import Campaigns
+from .brandings import Brandings
 
 class Client(object):
     BASE_URL = '{}/api/3'
@@ -38,6 +39,7 @@ class Client(object):
         self.dealActivities = DealActivities(self)
         self.customobjects = CustomObjects(self)
         self.campaigns = Campaigns(self)
+        self.brandings = Brandings(self)
 
     def _get(self, endpoint, **kwargs):
         return self._request('GET', endpoint, **kwargs)
